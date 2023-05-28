58227ac463935207c645f902ba3539b70bc0b036
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-05-28 21:46:04 +0200

    Initial Commit

---
+++ FitnessGoalSetter/FitnessGoalSetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessGoalSetterApp.swift
+//  FitnessGoalSetter
+//
+//  Created by Stjepan Stojčević on 28.05.2023..
+//
+
+import SwiftUI
+
+@main
+struct FitnessGoalSetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
