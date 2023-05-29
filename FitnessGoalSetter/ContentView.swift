75c9d423d2ba5480202bee41f5d28fbf8af65de4
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-05-29 23:12:51 +0200

    Popravljeni buttoni, dodan goal od kojeg se oduzima. Dodan zaseban ekran za glute bridge identičan kao za plank, ali sa zasebnim varijablama

--- FitnessGoalSetter/ContentView.swift
+++ FitnessGoalSetter/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var timeRemaining : Double = 120
-    @State private var isTimerRunning = false
+    @State private var timeRemainingPlank : Double = 120
+    @State private var timeRemainingGB : Double = 120
+    @State private var goalPlank : Double = 0
+    @State private var goalGB : Double = 0
+    @State private var isTimerRunningPlank = false
+    @State private var isTimerRunningGB = false
     @State private var prikaz1 = false
     @State private var prikaz2 = false
 
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    private let numberFormatter: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .none
+        return numberFormatter
+    }()
 
     var body: some View {
         VStack(spacing:10){
@@ -23,51 +33,43 @@ struct ContentView: View {
             }.sheet(isPresented: $prikaz1)
             {
                 Form{
-                    Section()
+                    Section("Plank")
                     {
-                        Button("Push ups"){
+                        Button("Glute Bridge"){
                             prikaz2.toggle()
                         }
-                        VStack {
-                            Text("Plank")
-                                .font(.title)
+            
+                        
+                            Text("Goal(in seconds) : ")
+                                .font(.title2)
                                 .padding()
-                            Spacer()
-                            
-                            Text("Time: \(String(format: "%.0f", timeRemaining)) sec")
+                        TextField("    ", value: $goalPlank, formatter: numberFormatter)
+                                    .fixedSize()
+                                    .keyboardType(.decimalPad)
+                            Text("Total time: \(String(format: "%.0f", goalPlank)) sec")
+                                .font(.title2)
+                                .padding().bold()
+                        
+                            Text("Time until the end of the current serie: \(String(format: "%.0f", timeRemainingPlank)) sec")
                                 .font(.title2)
                                 .padding()
                             
-                            Slider(value: $timeRemaining, in: 0...120, step: 1)
+                        
+                            Slider(value: $timeRemainingPlank, in: 0...120, step: 1)
                                 .padding()
                                 .accentColor(.red)
-                            Spacer()
-                            HStack{
-                                Spacer()
-                                VStack(spacing: 20){
-                                    Button(action: self.set(num: 30.0),label: {Text("30  sec")
-                                        .foregroundColor(Color.yellow)}).frame(width: 180, height: 40).background(Color.black)
+                            Button(action: self.setPlank(num: 30.0),label: {Text("30  sec")
+                                        .foregroundColor(Color.yellow)}).frame(width: 350, height: 40).background(Color.black)
                                         .cornerRadius(10)
-                                    
-                                    Button(action: self.set(num: 90.0),label: {Text("90  sec")
-                                        .foregroundColor(Color(hue: 0.073, saturation: 1.0, brightness: 1.0))}).frame(width: 180, height: 40).background(Color.black)
+                            Button(action: self.setPlank(num: 60.0),label: {Text("60  sec")
+                                        .foregroundColor(Color(hue: 0.111, saturation: 0.999, brightness: 0.917))}).frame(width: 350, height: 40).background(Color.black)
                                         .cornerRadius(10)
-                                }
-                                VStack(spacing: 20){
-                                    Button(action: self.set(num: 60.0),label: {Text("60  sec")
-                                        .foregroundColor(Color(hue: 0.111, saturation: 0.999, brightness: 0.917))}).frame(width: 180, height: 40).background(Color.black)
-                                        .cornerRadius(10)
-                                    Button(action: self.set(num: 120.0),label: {Text("120  sec")
-                                            .foregroundColor(Color(hue: 1.0, saturation: 1.0, brightness: 0.811))
-                                    }).frame(width: 180, height: 40).background(Color.black)
-                                        .cornerRadius(10)
-                                }
-                                Spacer()
-                            }
-                            Spacer()
-                            Button(action: {isTimerRunning.toggle()})
+                            Button(action: self.setPlank(num: 90.0),label: {Text("90  sec")
+                                .foregroundColor(Color.red)}).frame(width: 350, height: 40).background(Color.black)
+                                    .cornerRadius(10)
+                            Button(action: {isTimerRunningPlank.toggle()})
                             {
-                                Text(isTimerRunning ? "Pause" : "Start")
+                                Text(isTimerRunningPlank ? "Pause" : "Start")
                                     .font(.title)
                                     .padding()
                                     .background(Color.red)
@@ -75,10 +77,11 @@ struct ContentView: View {
                                     .cornerRadius(10)
                             }}
                         .onReceive(timer) { _ in
-                            if isTimerRunning && timeRemaining > 0 {
-                                timeRemaining -= 1
+                            if isTimerRunningPlank && timeRemainingPlank > 0 {
+                                timeRemainingPlank = timeRemainingPlank - 1
+                                goalPlank = goalPlank - 1
                             }
-                        }
+                        
                         Button("Done"){
                             prikaz1.toggle()
                         }
@@ -86,12 +89,51 @@ struct ContentView: View {
                     
                 }.sheet(isPresented: $prikaz2){
                     Form{
-                        Section("Push Ups")
+                        Section("Glute Bridge")
                         {
-                            Button("Done All"){
-                                prikaz1.toggle()
-                                prikaz2.toggle()
+                            Text("Goal(in seconds) : ")
+                                .font(.title2)
+                                .padding()
+                        TextField("    ", value: $goalGB, formatter: numberFormatter)
+                                    .fixedSize()
+                                    .keyboardType(.decimalPad)
+                            Text("Total time: \(String(format: "%.0f", goalGB)) sec")
+                                .font(.title2)
+                                .padding().bold()
+                        
+                            Text("Time until the end of the current serie: \(String(format: "%.0f", timeRemainingGB)) sec")
+                                .font(.title2)
+                                .padding()
+                            
+                        
+                            Slider(value: $timeRemainingGB, in: 0...120, step: 1)
+                                .padding()
+                                .accentColor(.red)
+                            Button(action: self.setGB(num: 30.0),label: {Text("30  sec")
+                                        .foregroundColor(Color.yellow)}).frame(width: 350, height: 40).background(Color.black)
+                                        .cornerRadius(10)
+                            Button(action: self.setGB(num: 60.0),label: {Text("60  sec")
+                                        .foregroundColor(Color(hue: 0.111, saturation: 0.999, brightness: 0.917))}).frame(width: 350, height: 40).background(Color.black)
+                                        .cornerRadius(10)
+                            Button(action: self.setGB(num: 90.0),label: {Text("90  sec")
+                                .foregroundColor(Color.red)}).frame(width: 350, height: 40).background(Color.black)
+                                    .cornerRadius(10)
+                            Button(action: {isTimerRunningGB.toggle()})
+                            {
+                                Text(isTimerRunningGB ? "Pause" : "Start")
+                                    .font(.title)
+                                    .padding()
+                                    .background(Color.red)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }}
+                        .onReceive(timer) { _ in
+                            if isTimerRunningGB && timeRemainingGB > 0 {
+                                timeRemainingGB = timeRemainingGB - 1
+                                goalGB = goalGB - 1
                             }
+                        
+                       
                             
                         }
                     }
@@ -100,9 +142,13 @@ struct ContentView: View {
             
         }}
     
-    func set(num: Double) -> () -> Void
+    func setPlank(num: Double) -> () -> Void
         {
-            return {timeRemaining=num}
+            return {timeRemainingPlank=num}
+        }
+    func setGB(num: Double) -> () -> Void
+        {
+            return {timeRemainingGB=num}
         }
 }
 
